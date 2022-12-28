1b54cb46fca254178950dc7ecbee773a8b88c712
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-12-28 09:31:15 +0530

    (Shubham)Refactor: Calculating Employee Daily Wage using function for Part Time and Full Time work

--- empWage.js
+++ empWage.js
@@ -1,24 +1,24 @@
-//UC2-employee wage
+//UC3-Refactor uc2 to cal employee wage using Function
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const WAGE_PER_HR = 20;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 let empCheck = Math.floor(Math.random() * 3 );  // check for attendence
-let empWage, workingHours;          //variables
-switch(empCheck){
-    case IS_FULL_TIME:
-        workingHours = FULL_TIME_HOURS;
-        console.log("Employee is present for full-time");
-        break;
-    case IS_PART_TIME:
-        workingHours = PART_TIME_HOURS;
-        console.log("Employee is Present for part-time");
-        break;
-    default:
-        console.log("Employee is Absent");
-        workingHours = 0;
-        break;
+let empWage = WAGE_PER_HR * GetWorkingHrs(empCheck);
+console.log("Employee wage for this day is "+ empWage);
+
+//Method to get Work Hours
+function GetWorkingHrs(empCheck){
+    switch(empCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+            break;
+        case IS_FULL_TIME:
+            return FULL_DAY_HOURS;
+            break;
+        default:
+            return 0;
+            break;
+    }
 }
-empWage = workingHours * WAGE_PER_HR;
-console.log("Employee Wage: " + empWage);

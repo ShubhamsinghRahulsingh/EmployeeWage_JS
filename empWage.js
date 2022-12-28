d573a339bdb38158b215fe1587f2b1acbcaee29c
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-12-28 09:09:20 +0530

    (Shubham)Add: Calculating Employee Daily Wage for Part Time and Full Time work

--- empWage.js
+++ empWage.js
@@ -1,10 +1,24 @@
-//UC1-Employee is present or absent
-
-const IS_PRESENT=0;
-let empCheck=Math.floor(Math.random()*10)%2;
-if(empCheck==IS_PRESENT)
-{
-    console.log("Employee is PRESENT");
+//UC2-employee wage
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const WAGE_PER_HR = 20;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+let empCheck = Math.floor(Math.random() * 3 );  // check for attendence
+let empWage, workingHours;          //variables
+switch(empCheck){
+    case IS_FULL_TIME:
+        workingHours = FULL_TIME_HOURS;
+        console.log("Employee is present for full-time");
+        break;
+    case IS_PART_TIME:
+        workingHours = PART_TIME_HOURS;
+        console.log("Employee is Present for part-time");
+        break;
+    default:
+        console.log("Employee is Absent");
+        workingHours = 0;
+        break;
 }
-else
-console.log("Employee is ABSENT");
+empWage = workingHours * WAGE_PER_HR;
+console.log("Employee Wage: " + empWage);
